26787ddf8da4c6bcc0f1762f6f05747fd43bb4ce
NehaGuptha <56723251+NehaGuptha@users.noreply.github.com>
2020-08-03 21:07:27 -0700

    minor

--- lib/services/Address_model.dart
+++ lib/services/Address_model.dart
@@ -7,21 +7,6 @@ class address {
 
   address({this.status,this.message,this.data});
 
-  factory address.fromJson(Map<String, dynamic> parsedJson){
-
-    var list = parsedJson['images'] as List;
-    print(list.runtimeType);
-    List<Data> dataList = list.map((i) => Data.fromJson(i)).toList();
-
-
-    return address(
-        status: parsedJson['status'],
-        message: parsedJson['message'],
-        data: dataList
-
-    );
-  }
-
 }
 
 class Data {
@@ -38,18 +23,7 @@ class Data {
   Data({this.id, this.name, this.mobilenumber, this.address, this.city,
       this.distric, this.state, this.pincode});
 
-  factory Data.fromJson(Map<String,dynamic> parsedJson){
-    return Data(
-      id: parsedJson['id'],
-      name: parsedJson['name'],
-      mobilenumber: parsedJson['mobilenumber'],
-      address: parsedJson['address'],
-      city: parsedJson['city'],
-      distric: parsedJson['distric'],
-        state: parsedJson['state'],
-        pincode: parsedJson['pincode']
-    );
-  }
+ 
 
 
 }
